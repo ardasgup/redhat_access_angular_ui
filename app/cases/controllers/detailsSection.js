2188892dfe80c5282d373212a707d82776f5c497
Aritro Dasgupta <ardasgup@redhat.com>
2014-10-30 17:20:04 +0530

    second pass

--- app/cases/controllers/detailsSection.js
+++ app/cases/controllers/detailsSection.js
@@ -4,13 +4,15 @@ angular.module('RedhatAccess.cases').controller('DetailsSection', [
     '$scope',
     'strataService',
     'CaseService',
+    'securityService',
     '$rootScope',
     'AUTH_EVENTS',
     'CASE_EVENTS',
     'AlertService',
     'RHAUtils',
-    function ($scope, strataService, CaseService, $rootScope, AUTH_EVENTS, CASE_EVENTS, AlertService, RHAUtils) {
+    function ($scope, strataService, CaseService, securityService, $rootScope, AUTH_EVENTS, CASE_EVENTS, AlertService, RHAUtils) {
         $scope.CaseService = CaseService;
+        $scope.securityService = securityService;
         $scope.init = function () {
             if (!$scope.compact) {
                 strataService.values.cases.types().then(function (response) {
@@ -39,61 +41,82 @@ angular.module('RedhatAccess.cases').controller('DetailsSection', [
             }, function (error) {
                 AlertService.addStrataErrorMessage(error);
             });
+            
+            $scope.userIsCaseOwner = true;
+            var ownerOptions = [];
+            var fullName = securityService.loginStatus.authedUser.first_name+' '+securityService.loginStatus.authedUser.last_name;
+            if (fullName !== CaseService.kase.owner && securityService.loginStatus.authedUser.is_internal){
+                $scope.userIsCaseOwner = false;
+                ownerOptions.push({
+                    value: securityService.loginStatus.authedUser.sso_username,
+                    label: fullName
+                },{
+                    value: CaseService.kase.owner,
+                    label: CaseService.kase.owner
+                });
+            }
+            $scope.owners = ownerOptions;
         };
         $scope.updatingDetails = false;
         $scope.updateCase = function () {
             $scope.updatingDetails = true;
             var caseJSON = {};
-            if (CaseService.kase !== undefined) {
-                if (CaseService.kase.type !== undefined) {
-                    caseJSON.type = CaseService.kase.type.name;
-                }
-                if (CaseService.kase.severity !== undefined) {
-                    caseJSON.severity = CaseService.kase.severity.name;
-                }
-                if (CaseService.kase.status !== undefined) {
-                    caseJSON.status = CaseService.kase.status.name;
-                }
-                if (CaseService.kase.alternate_id !== undefined) {
-                    caseJSON.alternateId = CaseService.kase.alternate_id;
-                }
-                if (CaseService.kase.product !== undefined) {
-                    caseJSON.product = CaseService.kase.product.name;
-                }
-                if (CaseService.kase.version !== undefined) {
-                    caseJSON.version = CaseService.kase.version;
-                }
-                if (CaseService.kase.summary !== undefined) {
-                    caseJSON.summary = CaseService.kase.summary;
-                }
-                if (CaseService.kase.group !== null && CaseService.kase.group !== undefined && CaseService.kase.group.number !== undefined) {
-                    caseJSON.folderNumber = CaseService.kase.group.number;
-                } else {
-                    caseJSON.folderNumber = '';
-                }
-                if (RHAUtils.isNotEmpty(CaseService.kase.fts)) {
-                    caseJSON.fts = CaseService.kase.fts;
-                    if (!CaseService.kase.fts) {
-                        caseJSON.contactInfo24X7 = '';
+            if ($scope.caseDetailsChanged() === true) {
+                if (CaseService.kase !== undefined) {
+                    if (CaseService.kase.type !== undefined) {
+                        caseJSON.type = CaseService.kase.type.name;
                     }
-                }
-                if (CaseService.kase.fts && RHAUtils.isNotEmpty(CaseService.kase.contact_info24_x7)) {
-                    caseJSON.contactInfo24X7 = CaseService.kase.contact_info24_x7;
-                }
-                if (CaseService.kase.notes !== null) {
-                    caseJSON.notes = CaseService.kase.notes;
-                }
-                strataService.cases.put(CaseService.kase.case_number, caseJSON).then(function () {
-                    $scope.caseDetails.$setPristine();
-                    $scope.updatingDetails = false;
-                    if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+                    if (CaseService.kase.severity !== undefined) {
+                        caseJSON.severity = CaseService.kase.severity.name;
+                    }
+                    if (CaseService.kase.status !== undefined) {
+                        caseJSON.status = CaseService.kase.status.name;
+                    }
+                    if (CaseService.kase.alternate_id !== undefined) {
+                        caseJSON.alternateId = CaseService.kase.alternate_id;
+                    }
+                    if (CaseService.kase.product !== undefined) {
+                        caseJSON.product = CaseService.kase.product.name;
+                    }
+                    if (CaseService.kase.version !== undefined) {
+                        caseJSON.version = CaseService.kase.version;
+                    }
+                    if (CaseService.kase.summary !== undefined) {
+                        caseJSON.summary = CaseService.kase.summary;
+                    }
+                    if (CaseService.kase.group !== null && CaseService.kase.group !== undefined && CaseService.kase.group.number !== undefined) {
+                        caseJSON.folderNumber = CaseService.kase.group.number;
+                    } else {
+                        caseJSON.folderNumber = '';
+                    }
+                    if (RHAUtils.isNotEmpty(CaseService.kase.fts)) {
+                        caseJSON.fts = CaseService.kase.fts;
+                        if (!CaseService.kase.fts) {
+                            caseJSON.contactInfo24X7 = '';
+                        }
+                    }
+                    if (CaseService.kase.fts && RHAUtils.isNotEmpty(CaseService.kase.contact_info24_x7)) {
+                        caseJSON.contactInfo24X7 = CaseService.kase.contact_info24_x7;
+                    }
+                    if (CaseService.kase.notes !== null) {
+                        caseJSON.notes = CaseService.kase.notes;
+                    }
+                    strataService.cases.put(CaseService.kase.case_number, caseJSON).then(function () {
+                        $scope.caseDetails.$setPristine();
+                        $scope.updatingDetails = false;
+                        if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+                            $scope.$apply();
+                        }
+                    }, function (error) {
+                        AlertService.addStrataErrorMessage(error);
+                        $scope.updatingDetails = false;
                         $scope.$apply();
-                    }
-                }, function (error) {
-                    AlertService.addStrataErrorMessage(error);
-                    $scope.updatingDetails = false;
-                    $scope.$apply();
-                });
+                    });
+                }
+            }
+            
+            if ($scope.caseDetails.owner !== undefined && $scope.caseDetails.owner.$dirty) {
+                $scope.changeCaseOwner();
             }
         };
         $scope.getProductVersions = function () {
@@ -105,13 +128,29 @@ angular.module('RedhatAccess.cases').controller('DetailsSection', [
             });
         };
         $scope.changeCaseOwner = function () {
-            $scope.updatingDetails = true;
             strataService.cases.owner.update(CaseService.kase.case_number,CaseService.kase.owner).then(function () {
+                CaseService.kase.owner = securityService.loginStatus.authedUser.first_name+' '+securityService.loginStatus.authedUser.last_name;
+                $scope.userIsCaseOwner = true;
                 $scope.updatingDetails = false;
+                $scope.caseDetails.$setPristine();
+                if ($scope.$root.$$phase !== '$apply' && $scope.$root.$$phase !== '$digest') {
+                    $scope.$apply();
+                }
             }, function (error) {
                 $scope.updatingDetails = false;
                 AlertService.addStrataErrorMessage(error);
             });
+        };
+        $scope.caseDetailsChanged = function () {
+            if ($scope.caseDetails.summary.$dirty || $scope.caseDetails.type.$dirty || $scope.caseDetails.severity.$dirty
+                    || $scope.caseDetails.status.$dirty || $scope.caseDetails.alternate_id.$dirty || $scope.caseDetails.product.$dirty
+                    || $scope.caseDetails.version.$dirty || $scope.caseDetails.group.$dirty || ($scope.caseDetails.notes !== undefined && $scope.caseDetails.notes.$dirty)
+                    || ($scope.caseDetails.ftsCheckbox !== undefined && $scope.caseDetails.ftsCheckbox.$dirty)
+                    || ($scope.caseDetails.ftsContact !== undefined && $scope.caseDetails.ftsContact.$dirty)) {
+                return true;
+            } else {
+                return false;
+            }
         };
         if (CaseService.caseDataReady) {
             $scope.init();
