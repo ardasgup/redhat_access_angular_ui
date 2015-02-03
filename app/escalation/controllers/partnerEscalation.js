2cf0f37d419279f045f3598d2368db63f2ab10b1
Aritro Dasgupta <ardasgup@redhat.com>
2015-02-03 14:54:20 +0530

    third pass partner escalation request

--- app/escalation/controllers/partnerEscalation.js
+++ app/escalation/controllers/partnerEscalation.js
@@ -14,26 +14,8 @@ angular.module('RedhatAccess.cases').controller('PartnerEscalation', [
     function ($scope, strataService, $stateParams, CaseService, EscalationRequestService, $rootScope, RHAUtils, AUTH_EVENTS, CASE_EVENTS, securityService, AlertService) {
         $scope.EscalationRequestService = EscalationRequestService;
         $scope.disableSendRequest = true;
-        $scope.geoList = [];
+        $scope.geoList = ['NA','EMEA','LATAM','APAC','None'];        
         
-        $scope.init = function () {
-            $scope.geoList.push({
-                value: 'NA',
-                label: 'NA'
-            }, {
-                value: 'EMEA',
-                label: 'EMEA'
-            }, {
-                value: 'LATAM',
-                label: 'LATAM'
-            }, {
-                value: 'APAC',
-                label: 'APAC'
-            }, {
-                value: 'None',
-                label: 'None'
-            });
-        };
         $scope.submitEscalationRequest = function() {
             EscalationRequestService.sendEscalationRequest();            
         }
@@ -44,9 +26,8 @@ angular.module('RedhatAccess.cases').controller('PartnerEscalation', [
                 $scope.disableSendRequest = true;
             }
         }
-        $scope.init();
+        
         $rootScope.$on(AUTH_EVENTS.loginSuccess, function () {
-            $scope.init();
             AlertService.clearAlerts();
         });
     }
